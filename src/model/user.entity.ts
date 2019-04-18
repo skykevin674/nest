d4641cb52facbf69f856c6cd87bdcbc135c121b0
xuchao <xuchao@anbai.com>
2019-04-18 18:00:22 +0800

    add transformer

--- src/model/user.entity.ts
+++ src/model/user.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class User {
@@ -8,9 +9,11 @@ export class User {
   @Column({ length: 100, nullable: false, unique: true, name: 'username' })
   username: string;
 
+  @Exclude()
   @Column()
   salt: string;
 
+  @Exclude()
   @Column({ length: 255, name: 'password_digest' })
   passwordDigest: string;
 }
